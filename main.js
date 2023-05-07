a659d7d9d1429d3df76fe6a4624eae561cdfe2e8
maxivillegas <villegasmaxi@gmail.com>
2023-05-07 19:05:47 -0300

    functionCorrer

--- main.js
+++ main.js
@@ -1,35 +1,36 @@
+function corre() {
+  function calculoRomana(numAmigos, costoCena) {
+    let montoRecaudado = 0;
 
-function corre(){
-function calculoRomana(numAmigos, costoCena) {
-  let montoRecaudado = 0;
-  
-  function rondaDeDinero(numAmigos, costoCena) {
-    let montoRonda = 0;
-    for (let i = 0; i < numAmigos; i++) {
-      const dineroFaltante = costoCena - montoRonda
-      const comenzal = prompt("Hola amigo: " + (i + 1) + " la cena costo " + parseInt(costoCena) + " y todavía falta poner " + dineroFaltante + " entre " + (numAmigos - i) + ". Cuanto podes poner vos?")
-      
-      console.log(" montoRecaudado: ", montoRonda);
-      montoRonda = montoRonda + parseInt(comenzal);
+    function rondaDeDinero(numAmigos, costoCena) {
+      let montoRonda = 0;
+      for (let i = 0; i < numAmigos; i++) {
+        const dineroFaltante = costoCena - montoRonda;
+        const comenzal = prompt(
+          "Hola amigo: " +(i + 1) +" la cena costo " +parseInt(costoCena) +" y falta poner " +dineroFaltante +" entre " 
+          +(numAmigos - i) + " Cuanto podes poner vos?");
+
+          montoRonda = montoRonda + parseInt(comenzal);
+        console.log(" montoRecaudado: ", montoRonda);
+        
+      }
+      if (montoRonda < costoCena) {
+        alert("No llegamos a cubrir el costo: Vamos de nuevo.");
+      }
+      return montoRonda;
     }
-    if (montoRonda < costoCena) {
-      alert("No llegamos a cubrir el costo: Vamos de nuevo.");
+    // Si no alcanza mando alerta "No llegamos a cubrir el costo: Vamos de nuevo." Y vuelvo a preguntar a todos cuanto quieren poner.
+
+    while (montoRecaudado < costoCena) {
+      montoRecaudado = rondaDeDinero(numAmigos, costoCena);
+      console.log(" Total recaudado en la romana: ", montoRecaudado);
     }
-    return montoRonda;
+    // Si al final de la vuelta alcanza, mando alerta "A comer" y con lo que sobra agrego "Quedo X para propina."
+    let propina = montoRecaudado - costoCena;
+    return alert("Genial, juntamos " + montoRecaudado + ". Y nos quedo " + propina +" para la propina.");
   }
-  // Si no alcanza mando alerta "No llegamos a cubrir el costo: Vamos de nuevo." Y vuelvo a preguntar a todos cuanto quieren poner.
 
-  while (montoRecaudado < costoCena) {
-    montoRecaudado = rondaDeDinero(numAmigos, costoCena);
-    console.log(" montoRecaudado en while: ", montoRecaudado);
-  }
-  // Si al final de la vuelta alcanza, mando alerta "A comer" y con lo que sobra agrego "Quedo X para propina."
-  let propina = montoRecaudado - costoCena
-  return alert("Genial, juntamos " + montoRecaudado + ". Y nos quedo " + propina + " para la propina." );
-}
-
-const numAmigos = prompt("Cuantos somos para cenar?");
-const costoCena = prompt("Cuanto costo la cena?");
-const montoRecaudado = calculoRomana(numAmigos, costoCena);
-
+  const numAmigos = prompt("Cuantos somos para cenar?");
+  const costoCena = prompt("Cuanto costo la cena?");
+  montoRecaudado = calculoRomana(numAmigos, costoCena);
 }
